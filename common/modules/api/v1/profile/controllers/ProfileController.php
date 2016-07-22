eb3b9052dd7c9f472460f1f70c844ad1c0b9dc93
Leo24 <leo2410@yandex.ru>
2016-07-22 16:02:29 +0300

    Amend controller names

---
+++ common/modules/api/v1/profile/controllers/ProfileController.php
@@ -0,0 +1,34 @@
+<?php
+namespace common\modules\api\v1\profile\controllers;
+
+use Yii;
+use yii\base\InvalidParamException;
+use yii\web\BadRequestHttpException;
+use yii\web\Controller;
+use yii\filters\VerbFilter;
+use yii\filters\AccessControl;
+use common\models\LoginForm;
+use frontend\models\PasswordResetRequestForm;
+use frontend\models\ResetPasswordForm;
+use frontend\models\SignupForm;
+use frontend\models\ContactForm;
+
+/**
+ * Profile controller
+ */
+class ProfileController extends Controller
+{
+
+    /**
+     * Displays test json.
+     *
+     * @return mixed
+     */
+    public function actionIndex()
+    {
+//        return $this->render('html');
+        return "{\"id\":\"ProfileController \"}";
+
+    }
+
+}
---
+++ common/modules/api/v1/sleepcycle/Module.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace common\modules\api\v1\sleepcycle;
+
+/**
+ * Class Module
+ * @package common\modules\api\v1\sleepcycle
+ */
+class Module extends \yii\base\Module
+{
+    public $controllerNamespace = 'common\modules\api\v1\sleepcycle\controllers';
+
+    /**
+     * @var string Major API version. Property should be filled
+     */
+    public $apiVersion = '1';
+}
