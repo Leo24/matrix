a19a44165155bc8b4f22631e70dd69f76aa418e7
Dmitriy Sobolevskiy <dmitriy.sobolevskiy@gmail.com>
2016-07-26 10:53:13 +0300

    Created migration for block table

---
+++ console/migrations/m160726_064733_create_block_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use yii\db\Schema;
+use yii\db\Migration;
+
+/**
+ * Handles the creation for table `block`.
+ */
+class m160726_064733_create_block_table extends Migration
+{
+    /**
+     * @inheritdoc
+     */
+    public function up()
+    {
+        $tableOptions = null;
+        if ($this->db->driverName === 'mysql') {
+            // http://stackoverflow.com/questions/766809/whats-the-difference-between-utf8-general-ci-and-utf8-unicode-ci
+            $tableOptions = 'CHARACTER SET utf8 COLLATE utf8_general_ci ENGINE=InnoDB';
+        }
+
+        $this->createTable('{{%block}}', [
+            'token' => $this->string(255),
+            'user_id' => $this->integer(11)->unsigned()->defaultValue(null),
+            'created_at' => Schema::TYPE_TIMESTAMP . ' DEFAULT CURRENT_TIMESTAMP',
+            'expired_at' => Schema::TYPE_TIMESTAMP. ' NULL',
+            'PRIMARY KEY(token)'
+        ], $tableOptions);
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function down()
+    {
+        $this->dropTable('{{%block}}');
+    }
+}
