5faaec92e8fed206489835868f69eea00a417b7c
Leo24 <leo2410@yandex.ru>
2016-07-27 10:57:00 +0300

    Amend receave Emfit data script

--- frontend/controllers/DataController.php
+++ frontend/controllers/DataController.php
@@ -9,7 +9,7 @@ use yii\rest\ActiveController;
 /**
  * Site controller
  */
-class ApiController extends ActiveController
+class DataController extends ActiveController
 {
 //    public $enableCsrfValidation = false;
     public $modelClass = 'app\models\VitalParams';
@@ -25,9 +25,9 @@ class ApiController extends ActiveController
             date_default_timezone_set('europe/kiev');
             $filename = date('Y:m:d-H:i:s', time());
             $myfile = fopen(getcwd() . '/tmp/' . $filename . ".txt", "w");
-            fwrite($myfile, 'File created!!!)))'."\n");
+//            fwrite($myfile, 'File created!!!)))'."\n");
             fwrite($myfile, $data."\n");
-            fwrite($myfile, 'End of input' . "\n");
+//            fwrite($myfile, 'End of input' . "\n");
             fclose($myfile);
             return 'Json Saved';
         }
