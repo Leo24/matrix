0e6601c336401b5eea3caae683678470774bdca9
Leo24 <leo2410@yandex.ru>
2016-07-29 13:44:27 +0300

    Created migrations for refactored tables

---
+++ console/migrations/m160729_090337_sleep_data_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use yii\db\Migration;
+
+class m160729_090337_sleep_data_table extends Migration
+{
+    /**
+     * @inheritdoc
+     */
+    public function up()
+    {
+        $tableOptions = null;
+        if ($this->db->driverName === 'mysql') {
+            // http://stackoverflow.com/questions/766809/whats-the-difference-between-utf8-general-ci-and-utf8-unicode-ci
+            $tableOptions = 'CHARACTER SET utf8 COLLATE utf8_general_ci ENGINE=InnoDB';
+        }
+        $this->createTable('{{%sleep_data}}', [
+            'id' => $this->primaryKey(),
+            'user_id' => $this->integer(11)->notNull(),
+            'timestamp' => $this->timestamp()->notNull(),
+            'sleep_type' => $this->float(),
+        ], $tableOptions);
+        $this->addForeignKey('fk_tbl_sleep_data_tbl_users',
+            '{{%sleep_data}}', 'user_id',
+            '{{%users}}', 'id',
+            'CASCADE', 'CASCADE');
+    }
+    /**
+     * @inheritdoc
+     */
+    public function down()
+    {
+        $this->dropForeignKey('fk_tbl_sleep_data_tbl_users', '{{%sleep_data}}');
+        $this->dropTable('{{%sleep_data}}');
+    }
+}
