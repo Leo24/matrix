71bbe82eee9b74eac96252b1ab66f2285819c1a1
Dmitriy Sobolevskiy <dmitriy.sobolevskiy@gmail.com>
2016-08-10 15:43:25 +0300

    Added migration for health table

---
+++ console/migrations/m160810_122247_create_health_table.php
@@ -0,0 +1,52 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Handles the creation for table `health`.
+ *
+ * @author Dmitriy Sobolevskiy <d.sabaleuski@andersenlab.com>
+ */
+class m160810_122247_create_health_table extends Migration
+{
+    /**
+     * @inheritdoc
+     */
+    public function safeUp()
+    {
+        $options = ($this->db->driverName === 'mysql')
+            ? 'CHARACTER SET utf8 COLLATE utf8_general_ci ENGINE=InnoDB' : null;
+
+        $this->createTable('{{%health}}', [
+            'id'                       => $this->primaryKey(),
+            'user_id'                  => $this->integer(11)->unique()->notNull(),
+            'weight'                   => $this->integer(3)->unsigned()->defaultValue(null),
+            'height'                   => $this->decimal()->unsigned()->defaultValue(null),
+            'blood_type'               => "enum('A','B', 'AB', 'O') DEFAULT NULL",
+            'blood_pressure_systolic'  => $this->integer(3)->unsigned()->defaultValue(null),
+            'blood_pressure_diastolic' => $this->integer(3)->unsigned()->defaultValue(null),
+            'cholesterol_level'        => $this->integer(3)->unsigned()->defaultValue(null),
+        ], $options);
+
+        $this->createIndex('idx-health-user_id', '{{%health}}', 'user_id');
+
+        $this->addForeignKey(
+            'fk-health-user_id',
+            '{{%health}}',
+            'user_id',
+            '{{%user}}',
+            'id',
+            'CASCADE'
+        );
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function safeDown()
+    {
+        $this->dropIndex('idx-health-user_id', '{{%health}}');
+        $this->dropForeignKey('fk-health-user_id', '{{%health}}');
+        $this->dropTable('{{%health}}');
+    }
+}
