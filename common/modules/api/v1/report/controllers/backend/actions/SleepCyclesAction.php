aaf0cec2825745c4cb61c71840eeac0a7c2fe58e
Leo24 <leo2410@yandex.ru>
2016-08-12 13:12:16 +0300

    Amend anotations

---
+++ common/modules/api/v1/report/controllers/backend/actions/SleepCyclesAction.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace common\modules\api\v1\report\controllers\backend\actions;
+
+use Yii;
+use yii\base\Exception;
+use yii\web\BadRequestHttpException;
+use yii\web\HttpException;
+use common\modules\api\v1\synchronize\models\SleepQuality;
+use \yii\rest\Action;
+use yii\web\ServerErrorHttpException;
+
+/**
+ * Class SleepCycles
+ * 
+ * Action for getting SleepCycles average data from last night and data for graph
+ *
+ * @package common\modules\api\v1\report\controllers\backend\actions
+ */
+class SleepCyclesAction extends Action
+{
+    /**
+     * @inheritdoc
+     */
+    public $modelClass = SleepQuality::class;
+
+    /**
+     * @return array with graph data
+     * @throws \yii\web\ServerErrorHttpException
+     * @throws \yii\web\BadRequestHttpException
+     */
+    public function run()
+    {
+        $graphData = [];
+        $params = \Yii::$app->request->queryParams;
+
+        if (!isset($params['user_id']) || !isset($params['startDate']) || !isset($params['endDate'])) {
+            throw new BadRequestHttpException('Params startDate, endDate and user_id are required.');
+        }
+
+        try {
+            /** @var  $sleepQualityModel SleepQuality.php */
+            $sleepQualityModel = new $this->modelClass;
+
+            $currentAverage = $sleepQualityModel->currentAverage($params);
+            $sleepQualityData = $sleepQualityModel->sleepQualityData($params);
+
+            if ($sleepQualityData) {
+                foreach ($sleepQualityData as $ln) {
+                    $graphData[] = [
+                        'chart' => [
+                            'axis_x'=> $ln['from'],
+                            'axis_y'=> $ln['sleep_score'],
+                        ],
+                    ];
+                }
+                $graphData[] = ['current_average' => $currentAverage];
+            }
+
+            return $graphData;
+
+        } catch (Exception $e) {
+            throw new ServerErrorHttpException('Failed to getting information for unknown reason.');
+        }
+    }
+}
