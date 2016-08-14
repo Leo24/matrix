dd2aa8ed6e2356aae44bece16a4e855cd69c6cf0
ratkus91 <a.sementzov@andersensoft.ru>
2016-08-14 11:55:02 +0300

    refactoring code for synchronize emfit data functionality

--- common/modules/api/v1/emfit/controllers/actions/synchronize/SaveAction.php
+++ common/modules/api/v1/emfit/controllers/actions/synchronize/SaveAction.php
@@ -12,6 +12,7 @@ use common\modules\api\v1\user\models\Device;
 use Yii;
 use yii\base\Exception;
 use yii\console\ErrorHandler;
+use yii\helpers\Json;
 use yii\rest\Action;
 use yii\web\NotFoundHttpException;
 use yii\web\ServerErrorHttpException;
@@ -64,50 +65,55 @@ class SaveAction extends Action
             $sleepQualityModel->saveSleepQualityData($json, $user->id);
 
             /** Saving Calc Data in DB */
-            if (!empty($json['calc_data'])) {
+            $calcData = Json::decode($json['calc_data']);
+            if (!empty($calcData)) {
 
                 /** @var  $calcDataModel CalcData.php */
                 $calcDataModel = new CalcData();
 
-                $calcDataModel->saveCalcData($json['calc_data'], $user->id);
+                $calcDataModel->saveCalcData($calcData, $user->id);
             }
 
             /** Saving Sleep Data in DB */
-            if (!empty($json['sleep_data'])) {
+            $sleepData = Json::decode($json['sleep_data']);
+            if (!empty($sleepData)) {
 
                 /** @var  $sleepDataModel SleepData.php */
                 $sleepDataModel = new SleepData();
 
-                $sleepDataModel->saveSleepData($json['sleep_data'], $user->id);
+                $sleepDataModel->saveSleepData($sleepData, $user->id);
 
             }
 
             /** Saving Hrv Data in DB */
-            if (!empty($json['hrv_data'])) {
+            $hrvData = Json::decode($json['hrv_data']);
+            if (!empty($hrvData)) {
 
                 /** @var  $hrvDataModel HrvData.php */
                 $hrvDataModel = new HrvData();
 
-                $hrvDataModel->saveHrvData($json['hrv_data'], $user->id, $json['from']);
+                $hrvDataModel->saveHrvData($hrvData, $user->id, $json['from']);
 
             }
 
             /** Saving Rmssd Data in DB */
-            if (!empty($json['hrv_rmssd_data'])) {
+            $hrvRmssdData = Json::decode($json['hrv_rmssd_data']);
+            if (!empty($hrvRmssdData)) {
 
                 /** @var  $hrvRmssdDataModel HrvRmssdData.php */
                 $hrvRmssdDataModel = new HrvRmssdData();
 
-                $hrvRmssdDataModel->saveRmssdData($json['hrv_rmssd_data'], $user->id);
+                $hrvRmssdDataModel->saveRmssdData($hrvRmssdData, $user->id);
             }
 
             /** Saving Movement information in DB */
-            if (!empty($json['tossnturn_data'])) {
+            $tossnturnData = Json::decode($json['tossnturn_data']);
+            if (!empty($tossnturnData)) {
 
                 /** @var  $movementModel Movement.php */
                 $movementModel = new Movement();
 
-                $movementModel->saveMovement($json['tossnturn_data'], $user->id);
+                $movementModel->saveMovement($tossnturnData, $user->id);
             }
 
             $transaction->commit();
